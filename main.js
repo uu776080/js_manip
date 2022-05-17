ea7caca7330e4e3383845a64d2a35a7db5d7c7c9
vv <vv@debian>
2022-05-17 18:00:15 +0500

    add files

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const btn6 = document.getElementById('+blue');
 const btn7 = document.getElementById('reset');
 const btn8 = document.getElementById('incH');
 const btn9 = document.getElementById('incW');
+const btn10 = document.getElementById('decH');
+const btn11 = document.getElementById('decW');
 
 let x = 0;
 let y = 0;
@@ -15,6 +17,8 @@ let w = 200;
 let h = 200;
 
 const info = document.getElementById('info');
+const info2 = document.getElementById('info2');
+const info3 = document.getElementById('info3');
 const chcol = document.getElementById('chcol');
 chng();
 wSize();
@@ -23,7 +27,7 @@ hSize();
 function chng() {
   //document.body.style.backgroundColor = 'rgb('+x+','+y+','+z+')';
   chcol.style.backgroundColor = 'rgb('+x+','+y+','+z+')';
-  info.textContent = 'color is ' + chcol.style.backgroundColor;
+  info.textContent = 'color: ' + chcol.style.backgroundColor;
   if (x>255) {
     x=255;
   } else if (x<0) {
@@ -47,9 +51,17 @@ function nega(e) {
 }
 function wSize() {
   chcol.style.width = w+'px';
+  info2.textContent = 'width: ' + chcol.style.width;
+  if (w<0) {
+    w=0;
+  }
 }
 function hSize() {
   chcol.style.height = h+'px';
+  info3.textContent = 'height: ' + chcol.style.height;
+  if (h<0) {
+    h=0;
+  }
 }
 function reset() {
   chng(x=0,y=0,z=0);
@@ -69,3 +81,5 @@ btn6.onclick = () => chng(z+=30);
 btn7.onclick = () => reset();
 btn9.onclick = () => wSize(w+=20);
 btn8.onclick = () => hSize(h+=20);
+btn11.onclick = () => wSize(w-=20);
+btn10.onclick = () => hSize(h-=20);
